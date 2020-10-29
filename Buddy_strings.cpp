f95fc4f0336076932bc8f46847bbacf115775dfb
Naman Adlakha <naman.adlakha3@gmail.com>
2020-10-30 00:19:19 +0530

    Create Buddy_strings.cpp

---
+++ Buddy_strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool buddyStrings(string a, string b) {
+      int c=0;
+        unordered_map<char,int>mp;
+        if(a==b)
+        {
+            for(int i=0;i<a.length();i++){if(mp[a[i]]!=0)return true;
+                                          mp[a[i]]++;}
+        }
+        for(int i=0;i<a.length();i++){if(a[i]!=b[i])c++;}
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(c==2&&a==b)
+            return true;
+        return false;
+    }
+};
