fa883d4bd8d007bd9bb30bf8eccc41e51ab4c491
Naman Adlakha <naman.adlakha3@gmail.com>
2020-10-30 00:16:59 +0530

    Create Rotate_array.cpp

---
+++ Rotate_array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int d) {
+        int n=arr.size();
+        
+ d = d % n; 
+        d=n-d;
+    int g_c_d = __gcd(d, n); 
+    for (int i = 0; i < g_c_d; i++) { 
+        /* move i-th values of blocks */
+        int temp = arr[i]; 
+        int j = i; 
+  
+        while (1) { 
+            int k = j + d; 
+            if (k >= n) 
+                k = k - n; 
+  
+            if (k == i) 
+                break; 
+  
+            arr[j] = arr[k]; 
+            j = k; 
+        } 
+        arr[j] = temp; 
+    } 
+    }
+};
