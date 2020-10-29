32e4bda020e5e7d888b0d63933df4b4713024dda
Naman Adlakha <naman.adlakha3@gmail.com>
2020-10-30 00:18:22 +0530

    Create Repeated_dna_sequences.cpp

---
+++ Repeated_dna_sequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int>mp;
+        vector<string>ans;
+       
+        for(int i=0;i<s.length();i++)
+        {
+            if(i+10<=s.length())
+            {
+        mp[s.substr(i,10)]++;
+            if(mp[s.substr(i,10)]==2)
+            {
+                ans.push_back(s.substr(i,10));
+             //  mp[s.substr(i,10)]=0; 
+            }}
+            else
+                break;
+            
+        }
+        return ans;
+    }
+};
