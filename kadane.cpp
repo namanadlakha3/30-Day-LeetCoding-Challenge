2734d3a38fbeddcca97953676830de8fcb925beb
Naman Adlakha <naman.adlakha3@gmail.com>
2023-11-01 00:00:54 +0530

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -7,7 +7,7 @@ int maxSubArraySum(int a[], int size)
     int max_so_far = INT_MIN, max_ending_here = 0,
        start =0, end = 0, s=0;
  
-    for (int i=0; i< size; i++ )
+    for (int i=0; i< size; i++ ) 
     {
         max_ending_here += a[i];
  
